7a29746f4bb012b106692a84590eb51f0752d8ad
William Muli <willi.wambu@gmail.com>
2023-07-28 14:22:32 +0300

    Added message form

---
+++ src/components/message-form.tsx
@@ -0,0 +1,171 @@
+import React from 'react'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as z from 'zod'
+import { Textarea } from '@/components/ui/textarea'
+import { Button } from '@/components/ui/button'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group'
+import { Paperclip, X } from 'lucide-react'
+import { useForm } from 'react-hook-form'
+
+const fileSchema =
+  typeof File !== 'undefined' ? z.instanceof(File).optional() : z.any()
+
+const formSchema = z.object({
+  message: z
+    .string()
+    .min(1, {
+      message: 'Message must be at least 1 character',
+    })
+    .or(z.literal(''))
+    .optional(),
+  file: fileSchema,
+  autoDeletePeriod: z.enum(['day', 'week', 'month'], {
+    required_error: 'You need to select auto delete period',
+  }),
+})
+export function MessageForm() {
+  // refs
+  const inputRef = React.useRef<HTMLInputElement>(null)
+
+  // hooks
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      message: '',
+      file: undefined,
+    },
+  })
+
+  // methods
+  const handleButtonClick = React.useCallback(() => {
+    inputRef.current?.click()
+  }, [inputRef])
+
+  const onSubmit = React.useCallback((values: z.infer<typeof formSchema>) => {
+    console.log(values)
+  }, [])
+
+  return (
+    <>
+      <h1 className="text-xl lg:text-2xl xl:text-3xl text-left py-8">
+        Your secret message
+      </h1>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            control={form.control}
+            name="message"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <div className="relative">
+                    <Button
+                      type="button"
+                      variant="ghost"
+                      size="icon"
+                      className="absolute left-[-1px] focus-visible:none hover:bg-transparent"
+                      onClick={handleButtonClick}
+                    >
+                      <Paperclip className="h-4 w-4" />
+                    </Button>
+                    <Textarea
+                      placeholder="Type your message or attach a file here"
+                      className="px-8"
+                      {...field}
+                    />
+                  </div>
+                </FormControl>
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="file"
+            render={() => (
+              <div className="grid w-full gap-1.5 relative">
+                <input
+                  type="file"
+                  className="hidden"
+                  onChange={(e) => {
+                    if (e.target.files?.length) {
+                      form.setValue('file', e.target.files[0])
+                    }
+                  }}
+                  ref={inputRef}
+                />
+                <div className="flex items-center flex-row gap-2 my-2 h-8">
+                  {form.getValues().file && (
+                    <>
+                      <p className="text-sm text-muted-foreground">
+                        {form.getValues().file.name}
+                      </p>
+                      <Button
+                        type="button"
+                        variant="ghost"
+                        size="sm"
+                        onClick={() => form.setValue('file', undefined)}
+                      >
+                        <X className="w-4 h-4" />
+                      </Button>
+                    </>
+                  )}
+                </div>
+              </div>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="autoDeletePeriod"
+            render={({ field }) => (
+              <FormItem className="">
+                <FormLabel className="flex justify-start lg:justify-center pb-2">
+                  Auto delete this message in:
+                </FormLabel>
+                <FormControl>
+                  <RadioGroup
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                    className="flex flex-col lg:flex-row w-full justify-start lg:justify-center lg:gap-10"
+                  >
+                    <FormItem className="flex items-center space-x-3 space-y-0">
+                      <FormControl>
+                        <RadioGroupItem value="day" />
+                      </FormControl>
+                      <FormLabel className="font-normal">1 day</FormLabel>
+                    </FormItem>
+                    <FormItem className="flex items-center space-x-3 space-y-0">
+                      <FormControl>
+                        <RadioGroupItem value="week" />
+                      </FormControl>
+                      <FormLabel className="font-normal">1 week</FormLabel>
+                    </FormItem>
+                    <FormItem className="flex items-center space-x-3 space-y-0">
+                      <FormControl>
+                        <RadioGroupItem value="month" />
+                      </FormControl>
+                      <FormLabel className="font-normal">1 month</FormLabel>
+                    </FormItem>
+                  </RadioGroup>
+                </FormControl>
+                <FormMessage className="flex justify-start lg:justify-center" />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="mt-10 w-full" size="lg">
+            Encrypt Message
+          </Button>
+        </form>
+      </Form>
+    </>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,46 +1,63 @@
+import React from 'react'
 import Head from 'next/head'
-import MainLayout from '../layouts/main';
+import MainLayout from '../layouts/main'
+import { MessageForm } from '@/components/message-form'
 
 export default function Home() {
   return (
     <MainLayout>
       <Head>
         <title>Secure Share</title>
-        <meta name='description' content='Share passwords securely.' />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='/favicon.ico' />
+        <meta name="description" content="Share passwords securely." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className='flex flex-col w-full h-[calc(100vh - 4rem)] lg:flex-row'>
-        <div className='flex justify-end order-2 w-full lg:order-1 lg:w-[50%]'>
-          <div className='flex flex-col w-[100%] justify-start pt-8 pl-12 pr-8 border-r 2xl:max-w-[754px]'>
-            <h1 className='text-left text-[36px]/8 my-8'>What we do</h1>
-            <p className='text-md mb-6 font-light'>Send secrets instantly and securely.</p>
+      <div className="flex flex-col w-full h-[calc(100vh-4rem)] lg:flex-row">
+        <div className="flex justify-end order-2 w-full lg:order-1 lg:w-[50%]">
+          <div className="flex flex-col w-[100%] justify-start pt-8 pl-12 pr-8 lg:border-r 2xl:max-w-[754px]">
+            <h1 className="text-left text-[36px]/8 my-8">What we do</h1>
+            <p className="text-md mb-6 font-light">
+              Send secrets instantly and securely.
+            </p>
             <ul className="list-none list-outside ml-7">
               <li className="pl-4 info-list-item relative">
-                <p className='text-lg leading-10 font-semibold'>Secure Communication</p>
-                <p className='text-md leading-8 font-light'>
-                  Our single-access messaging ensures private and confidential conversations. Track opened messages to see who accessed your chats.
+                <p className="text-lg leading-10 font-semibold">
+                  Secure Communication
+                </p>
+                <p className="text-md leading-8 font-light">
+                  Our single-access messaging ensures private and confidential
+                  conversations. Track opened messages to see who accessed your
+                  chats.
                 </p>
               </li>
-              <li className='mt-6 pl-4 info-list-item relative'>
-                <p className='text-lg leading-8 font-semibold'>Encrypted Browser Security</p>
-                <p className='text-md leading-8 font-light'>
-                  We use advanced browser encryption to keep your messages safe, making it impossible for hackers and even our team to access them.
+              <li className="mt-6 pl-4 info-list-item relative">
+                <p className="text-lg leading-8 font-semibold">
+                  Encrypted Browser Security
+                </p>
+                <p className="text-md leading-8 font-light">
+                  We use advanced browser encryption to keep your messages safe,
+                  making it impossible for hackers and even our team to access
+                  them.
                 </p>
               </li>
-              <li className='mt-6 pl-4 info-list-item relative'>
-                <p className='text-lg leading-8 font-semibold'>Protection from Breaches</p>
-                <p className='text-md leading-8 font-light'>
-                  Your information is untouchable from data breaches and leaks as the shared secrets expires after use.
+              <li className="mt-6 pl-4 info-list-item relative">
+                <p className="text-lg leading-8 font-semibold">
+                  Protection from Breaches
+                </p>
+                <p className="text-md leading-8 font-light">
+                  Your information is untouchable from data breaches and leaks
+                  as the shared secrets expires after use.
                 </p>
               </li>
             </ul>
           </div>
         </div>
-        <div className='flex justify-end order-1 w-full lg:order-1 lg:w-[50%]'>
-          some content on the right
+        <div className="flex order-1 w-full lg:order-1 lg:w-[50%]">
+          <div className="flex flex-col w-[100%] justify-start pt-8 pl-12 pr-8 2xl:max-w-[754px]">
+            <MessageForm />
+          </div>
         </div>
       </div>
     </MainLayout>
-  );
+  )
 }
