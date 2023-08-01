cff9bf3d665fbb202ca0f17b1345c1735a29ded0
William Muli <willi.wambu@gmail.com>
2023-08-01 16:22:37 +0300

    Fix prisma client instantiation

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,16 +1,11 @@
 import { PrismaClient } from '@prisma/client'
 
-const prisma: PrismaClient = new PrismaClient({
-  log: ['query', 'info', 'warn', 'error']
-})
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}
 
-// if (process.env.NODE_ENV === 'production') {
-//   prisma = new PrismaClient()
-// } else {
-//   if (!global.prisma) {
-//     global.prisma = new PrismaClient()
-//   }
-//   prisma = global.prisma
-// }
+const prisma = globalForPrisma.prisma ?? new PrismaClient()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
 
 export default prisma
