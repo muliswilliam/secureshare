08b1145649626ebeaab531349c15d97adea69001
William Muli <willi.wambu@gmail.com>
2023-09-19 16:46:27 +0300

    format

--- src/pages/api/chat/url.ts
+++ src/pages/api/chat/url.ts
@@ -1,17 +1,20 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { getLiveKitURL } from '../../../shared/utils';
+import { NextApiRequest, NextApiResponse } from 'next'
+import { getLiveKitURL } from '../../../shared/utils'
 
-export default async function handleServerUrl(req: NextApiRequest, res: NextApiResponse) {
+export default async function handleServerUrl(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   try {
-    const { region } = req.query;
+    const { region } = req.query
 
     if (Array.isArray(region)) {
-      throw Error('provide max one region string');
+      throw Error('provide max one region string')
     }
-    const url = getLiveKitURL(region);
-    res.status(200).json({ url });
+    const url = getLiveKitURL(region)
+    res.status(200).json({ url })
   } catch (e) {
-    res.statusMessage = (e as Error).message;
-    res.status(500).end();
+    res.statusMessage = (e as Error).message
+    res.status(500).end()
   }
 }
