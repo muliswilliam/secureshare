190f87e5ff780d38ad2e31ca7c20c84c67036387
William Muli <willi.wambu@gmail.com>
2023-09-24 15:59:32 +0300

    Fix thumbnail typo

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -14,7 +14,7 @@ export function AppHead () {
       />
       <meta name="og:title" content="Share Secrets Quickly and Securely | SecureShare" />
       <meta name="og:description" content="Experience the ultimate in online security with our top-rated encrypted messaging and chat room service. Enjoy robust password and file protection, complete with a self-destruct feature. Safeguard your sensitive data, utilize link tracking, and shield yourself from phishing attempts with our cutting-edge web app." />
-      <meta name="og:image" content="https://secureshare.sh/thumbanil.png" />
+      <meta name="og:image" content="https://secureshare.sh/thumbnail.png" />
     </Head>
   )
 }
