5644f1611a979feee3b20c13f543504951385f4f
William Muli <willi.wambu@gmail.com>
2023-09-24 15:51:36 +0300

    Added og tags

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -5,12 +5,16 @@ export function AppHead () {
   return (
     <Head>
       <title>Share Secrets Quickly and Securely | SecureShare</title>
+     
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
       <meta
         name="description"
         content="Experience the ultimate in online security with our top-rated encrypted messaging and chat room service. Enjoy robust password and file protection, complete with a self-destruct feature. Safeguard your sensitive data, utilize link tracking, and shield yourself from phishing attempts with our cutting-edge web app."
       />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.ico" />
+      <meta name="og:title" content="Share Secrets Quickly and Securely | SecureShare" />
+      <meta name="og:description" content="Experience the ultimate in online security with our top-rated encrypted messaging and chat room service. Enjoy robust password and file protection, complete with a self-destruct feature. Safeguard your sensitive data, utilize link tracking, and shield yourself from phishing attempts with our cutting-edge web app." />
+      <meta name="og:image" content="https://secureshare.sh/thumbanil.png" />
     </Head>
   )
 }
